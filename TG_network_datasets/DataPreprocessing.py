57f78c16468909d84740246eff7d51eb253dfe26
Chandima301 <chandimaamarasena.18@cse.mrt.ac.lk>
2023-03-05 12:50:27 +0530

    TG network preprocess code added

--- TG_network_datasets/DataPreprocessing.py
+++ TG_network_datasets/DataPreprocessing.py
@@ -1,27 +1,62 @@
-def create_dataset():
-    print("creating dataset")
-    # with open(data_name) as f:
-    #     s = next(f)  # skip the first line
-    #     for idx, line in enumerate(f):
-    #       e = line.strip().split(',')
-    #       u = int(e[0])  # user_id
-    #       i = int(e[1])  # item_id
-    #
-    #       ts = float(e[2])  # timestamp  --> assumed in ascending order (I've checked it)
-    #       label = float(e[3])  # int(e[3])  # state_label
-    #
-    #       feat = np.array([float(x) for x in e[4:]] + [0 for _ in range(n_node_feat - (4+1))])  # edge features
-    #
-    #       u_list.append(u)
-    #       i_list.append(i)
-    #       ts_list.append(ts)
-    #       label_list.append(label)
-    #       idx_list.append(idx)
-    #
-    #       feat_l.append(feat)
+import csv
+
+
+def read_dataset():
+    print("Preprocessing dataset")
+    edge_list = []
+    user_id_list = []
+    item_id_list = []
+    node_list = []
+    with open("../../TG_network_datasets/Flights/Flights.csv", "r") as f:
+        s = next(f)  # skip the first line
+        for idx, line in enumerate(f):
+            e = line.strip().split(',')
+            u = int(e[0])  # user_id
+            i = int(e[1])  # item_id
+
+            if u not in user_id_list:
+                user_id_list.append(u)
+            if i not in item_id_list:
+                item_id_list.append(i)
+
+            ts = float(e[2])  # timestamp  --> assumed in ascending order (I've checked it)
+
+            edge_list.append([u, i, ts])
+            node_list.extend(user_id_list)
+            node_list.extend(item_id_list)
+
+    return edge_list, node_list
+
+
+def preprocess_dataset():
+    n_node_feat = 8
+    edge_list, node_list = read_dataset()
+    node_list.sort()
+
+    node_features_list = []
+
+    # add node features
+    for node in node_list:
+        node_features_list.append([node] + [0] * n_node_feat)
+
+    save_dataset(edge_list, node_features_list)
+
+
+def save_dataset(edge_list, node_features_list):
+    print("Writing edgelist", len(edge_list))
+    with open("edgelist_with_timestamp.csv", "w+", newline='') as edge_file:
+        writer = csv.writer(edge_file, delimiter=",")
+        for edge in edge_list:
+            writer.writerow(edge)
+
+    print("Writing node features", len(node_features_list))
+    with open("node_features.csv", "w+", newline='') as node_file:
+        writer = csv.writer(node_file, delimiter=",")
+        for node in node_features_list:
+            writer.writerow(node)
 
 
 if __name__ == "__main__":
     paper_data = dict()
     paper_map = dict()
-    create_dataset()
+    preprocess_dataset()
