fb7bb106bbf242db84dccb1729d62acd35e48fdd
Chandima301 <chandimaamarasena.18@cse.mrt.ac.lk>
2023-03-08 22:36:37 +0530

    getting average rating for each timestamps

--- epinion/readDataset.py
+++ epinion/readDataset.py
@@ -35,12 +35,24 @@ for node in ratingstruct:
     except KeyError:
         user_rating[node[0]] = [node[3]]
 
+user_rating_helpfulness = {}
+for node in ratingstruct:
+    try:
+        user_rating_helpfulness[node[0]].append(node[4])
+    except KeyError:
+        user_rating_helpfulness[node[0]] = [node[4]]
+
 print("Writing node features", len(ratingstruct))
 
 with open("rating.csv", "w+", newline='') as node_file:
     writer = csv.writer(node_file, delimiter=",")
+    writer.writerow(["node", "column_1", "column_2"])
 
     for user_id in user_rating.keys():
-        all_user_helpfulness = user_rating[user_id]
+        all_user_rating = user_rating[user_id]
+        avg_rating = sum(all_user_rating) / len(all_user_rating)
+
+        all_user_helpfulness = user_rating_helpfulness[user_id]
         avg_helpfulness = sum(all_user_helpfulness)/len(all_user_helpfulness)
-        writer.writerow([user_id, avg_helpfulness])
+
+        writer.writerow([user_id, avg_rating, avg_helpfulness])
