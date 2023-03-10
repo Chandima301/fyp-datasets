7ea575a614da00f59478c7898ee3358f48961300
Chandima301 <chandimaamarasena.18@cse.mrt.ac.lk>
2023-03-10 16:05:08 +0530

    product categories added ro epinion dataset

--- epinion/readDataset.py
+++ epinion/readDataset.py
@@ -31,24 +31,29 @@ ratingstruct = mat_contents['rating']
 print(ratingstruct)
 
 user_rating = {}
+user_rating_helpfulness = {}
+user_categories = {}
+all_categories = set()
+
 for node in ratingstruct:
+    all_categories.add(node[2])
     try:
         user_rating[node[0]].append(node[3])
+        user_rating_helpfulness[node[0]].append(node[4])
+        user_categories[node[0]].append(node[2])
     except KeyError:
         user_rating[node[0]] = [node[3]]
-
-user_rating_helpfulness = {}
-for node in ratingstruct:
-    try:
-        user_rating_helpfulness[node[0]].append(node[4])
-    except KeyError:
         user_rating_helpfulness[node[0]] = [node[4]]
+        user_categories[node[0]] = [node[2]]
 
 print("Writing node features", len(ratingstruct))
 
 with open("rating.csv", "w+", newline='') as node_file:
     writer = csv.writer(node_file, delimiter=",")
-    writer.writerow(["node", "column_1", "column_2"])
+    first_line = ["node"]
+    for a in range(10+len(all_categories)):
+        first_line.append("column_"+str(a+1))
+    writer.writerow(first_line)
 
     for user_id in user_rating.keys():
 
@@ -56,13 +61,19 @@ with open("rating.csv", "w+", newline='') as node_file:
 
         rating_counts = Counter(user_rating[user_id])
         helpfulness_counts = Counter(user_rating_helpfulness[user_id])
+        category_counts = Counter(user_categories[user_id])
 
         # 5 values for rating
         for i in range(1, 6):
             feature_vector.append(round((rating_counts[i] / len(user_rating[user_id])), 3))
 
-        # 5 values for rating
+        # 5 values for helpfulness
         for j in range(1, 6):
             feature_vector.append(round((helpfulness_counts[j] / len(user_rating_helpfulness[user_id])), 3))
 
-        writer.writerow([user_id] + feature_vector)
+        product_category_feature_vector = []
+        # for product categories
+        for k in all_categories:
+            product_category_feature_vector.append(round((category_counts[k]/len(all_categories)), 3))
+
+        writer.writerow([user_id] + feature_vector + product_category_feature_vector)
