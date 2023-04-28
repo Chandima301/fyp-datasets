c982f85f8a526b566ae6e3e8feda279a7a63ffcd
Chandima301 <chandimaamarasena.18@cse.mrt.ac.lk>
2023-04-28 21:19:58 +0530

    author affiliation generation in dblp

---
+++ dblp.v14-co-author/Univercities.py
@@ -0,0 +1,43 @@
+import csv
+import orjson
+import gc
+from sys import argv
+
+
+def selected_paper_fos():
+    count = 0
+    author_affiliations = dict()
+    with open("../../datasets/dblp_v14.txt", "r") as dblp_file:
+        print("File opened")
+        for line in dblp_file:
+            print(line)
+            paper_dict = orjson.loads(line[:-2])
+            try:
+                authors = paper_dict["author"]
+            except KeyError:
+                # author key missing in data
+                continue
+
+            for f in authors:
+                author_name, author_affiliation, author_id = f.values()
+                try:
+                    author_affiliations[author_affiliation] += 1
+                except KeyError:
+                    author_affiliations[author_affiliation] = 1
+
+            if count % 100000 == 0:
+                # print(len(filtered_items), len(items))
+                gc.collect()
+
+                print(count)
+            count += 1
+
+    print("Writing author affiliations")
+    with open(f"author_affiliations.txt", "w+", newline='') as author_affiliations_file:
+        writer = csv.writer(author_affiliations_file, delimiter=" ")
+        for key, value in author_affiliations:
+            writer.writerow([key, value])
+
+
+if __name__ == "__main__":
+    selected_paper_fos()
